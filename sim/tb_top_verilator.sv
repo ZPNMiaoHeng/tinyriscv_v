c7a374acb8992644c013bbd92cab8eff0fd0007a
liangkangnan <liangkangnan@163.com>
2021-05-19 16:09:39 +0800

    temp commit
    Signed-off-by: liangkangnan <liangkangnan@163.com>

--- sim/tb_top_verilator.sv
+++ sim/tb_top_verilator.sv
@@ -1,16 +1,18 @@
-// Copyright 2018 Robert Balas <balasr@student.ethz.ch>
-// Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the "License"); you may not use this file except in
-// compliance with the License.  You may obtain a copy of the License at
-// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-// or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
-
-// Top level wrapper for a verilator RI5CY testbench
-// Contributor: Robert Balas <balasr@student.ethz.ch>
-
+ /*                                                                      
+ Copyright 2021 Blue Liang, liangkangnan@163.com
+                                                                         
+ Licensed under the Apache License, Version 2.0 (the "License");         
+ you may not use this file except in compliance with the License.        
+ You may obtain a copy of the License at                                 
+                                                                         
+     http://www.apache.org/licenses/LICENSE-2.0                          
+                                                                         
+ Unless required by applicable law or agreed to in writing, software    
+ distributed under the License is distributed on an "AS IS" BASIS,       
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and     
+ limitations under the License.                                          
+ */
 
 module tb_top_verilator #(
 
