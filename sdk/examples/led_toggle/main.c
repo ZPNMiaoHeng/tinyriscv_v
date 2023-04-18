6942faa82def58bcbf277314df8f5b7d44b7dcdb
liangkangnan <liangkangnan@163.com>
2023-04-18 14:54:54 +0800

    sdk:examples: update led_toggle
    Signed-off-by: liangkangnan <liangkangnan@163.com>

--- sdk/examples/led_toggle/main.c
+++ sdk/examples/led_toggle/main.c
@@ -6,15 +6,36 @@
 
 int main()
 {
-    // IO7用作GPIO7
-    pinmux_set_io7_func(IO7_GPIO7);
-    // gpio7输出模式
-    gpio_set_mode(GPIO7, GPIO_MODE_OUTPUT);
+    // IO10用作GPIO10
+    pinmux_set_io10_func(IO10_GPIO10);
+    // IO12用作GPIO12
+    pinmux_set_io12_func(IO12_GPIO12);
+    // IO14用作GPIO14
+    pinmux_set_io14_func(IO14_GPIO14);
+    // IO15用作GPIO15
+    pinmux_set_io15_func(IO15_GPIO15);
+
+    // 输出模式
+    gpio_set_mode(GPIO10, GPIO_MODE_OUTPUT);
+    gpio_set_mode(GPIO12, GPIO_MODE_OUTPUT);
+    gpio_set_mode(GPIO14, GPIO_MODE_OUTPUT);
+    gpio_set_mode(GPIO15, GPIO_MODE_OUTPUT);
 
     while (1) {
-        gpio_set_output_data(GPIO7, 1);  // GPIO7输出高
+        // 逐个点亮
+        gpio_set_output_data(GPIO12, 0);
         busy_wait(500000);
-        gpio_set_output_data(GPIO7, 0);  // GPIO7输出低
+        gpio_set_output_data(GPIO10, 0);
+        busy_wait(500000);
+        gpio_set_output_data(GPIO15, 0);
+        busy_wait(500000);
+        gpio_set_output_data(GPIO14, 0);
+        busy_wait(500000);
+        // 全灭
+        gpio_set_output_data(GPIO12, 1);
+        gpio_set_output_data(GPIO10, 1);
+        gpio_set_output_data(GPIO15, 1);
+        gpio_set_output_data(GPIO14, 1);
         busy_wait(500000);
     }
 }
